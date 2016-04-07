802d1b8e27d48add460c4d7fe115dd38eadc6ffa
dielduarte <magdiel.kesller@gmail.com>
2016-04-06 23:57:54 -0300

    added a itcss structure and refactoring jade tasks

--- tasks/jade.js
+++ tasks/jade.js
@@ -3,19 +3,7 @@ var minifyHTML = require('gulp-htmlmin');
 var jade = require('gulp-jade');
 var plumber = require('gulp-plumber');
 
-gulp.task('jade', ['generate-index'], function () {
-  var opts = {
-    comments: true
-  };
-
-  gulp.src(['./app/**/*.jade', '!./app/index.jade'])
-    .pipe(plumber())
-    .pipe(jade({pretty: true}))
-    .pipe(minifyHTML())
-    .pipe(gulp.dest('./build/'))
-});
-
-gulp.task('generate-index', function () {
+gulp.task('jade', function () {
   gulp.src('./app/index.jade')
     .pipe(plumber())
     .pipe(jade({pretty: true}))
